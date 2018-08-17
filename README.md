66db2ec04c3bd8f1357498450bb18e5cdae9b024
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2018-08-17 10:53:56 +0200

    INT: add c wrappers.

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-A simple c++ class to handler signals.
+A simple c++ class to handler signals. c wrappers are also available.
 The implementation is based on the sigwait() function.
 This function is called by a separate thread whose the only purpose is managing signals.
 The signals are masked in all other threads; thus no signal handler is automatically executed when a signal is received by the process. Signals should not be unmasked for any thread; otherwise it is undefined if the signal is manager by the thread that unmasked that signal or if the function sigwait() returns in the thread supposed to manage signals.
 
+C++ API:
+--------
 Registering an handler for a signal is done by creating a object of the class SignalHandler.
 
 Removing an hander is done by deleting the object.
 
 Several handlers can be registered for a same signal. In that case, handlers are executed in the order of registration.
+
+C WRAPPERS:
+----------
+c wrappers are available to link with c programs.
+In order to execute "make check", it is probably necessary to set the LIBS environment variable to contain the directive to link with the standard c++ library. For instance, on Ubuntu 18.04, LIBS must contains -lstdc++. Otherwise the c wrapper test program may not link because some symbols defined in the c++ standard library are missing. Adding explicit linking to the standard c++ library may be also needed when linking a c program that uses the wrappers.  
---
+++ test/test.c
@@ -0,0 +1,57 @@
+#include <signal_handler.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <memory.h>
+
+static int handler1Called = 0;
+static int handler2Called = 0;
+
+static void handler1() { handler1Called++; }
+
+static void handler2() {
+    if (3 != handler1Called)
+        exit(EXIT_FAILURE);
+    handler2Called++; 
+} 
+
+int main()
+{
+    handler_info_create(SIGINT, handler1); 
+
+    printf("Check handler is called.\n");
+    kill(getpid(), SIGINT);
+    for (int i = 0; i < 5 && 0 == handler1Called; i++)
+        sleep(1);
+    if (0 == handler1Called)
+        return EXIT_FAILURE;
+
+    printf("Check handler is called a second time.\n");
+    kill(getpid(), SIGINT);
+    for (int i = 0; i < 5 && 1 == handler1Called; i++)
+        sleep(1);
+    if (1 == handler1Called)
+        return EXIT_FAILURE;
+    printf("Check that two handlers are called.\n");
+    const int handler2_id = handler_info_create(SIGINT, handler2); 
+    kill(getpid(), SIGINT);
+    for (int i = 0; i < 5 && 2 == handler1Called && 0 == handler2Called; i++)
+        sleep(1);
+    if (2 == handler1Called)
+        return EXIT_FAILURE;
+    if (0 == handler2Called)
+        return EXIT_FAILURE;
+
+    printf("Check that handler can be removed.\n");
+    handler_info_delete(SIGINT, handler2_id); 
+    kill(getpid(), SIGINT);
+    for (int i = 0; i < 5 && 3 == handler1Called; i++)
+        sleep(1);
+    if (3 == handler1Called)
+        return EXIT_FAILURE;
+
+    printf("Check that signal with no registered handler can be received\n");
+    kill(getpid(), SIGTERM);
+    return EXIT_SUCCESS;
+}
